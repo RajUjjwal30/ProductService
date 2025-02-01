2e4e47cfda70d17287a4bc1820fafbebfbccd33d
RajUjjwal30 <ujjwal.2000.raj@gmail.com>
2025-02-01 14:55:15 +0530

    Added DataBase Connection

--- src/main/java/com/example/productservice/models/BaseModel.java
+++ src/main/java/com/example/productservice/models/BaseModel.java
@@ -1,13 +1,21 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@MappedSuperclass
 public class BaseModel {
+    @Id // for primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Auto Increment
     private long id;
     private Date CreatedAt;
     private Date UpdatedAt;
--- src/main/java/com/example/productservice/models/Product.java
+++ src/main/java/com/example/productservice/models/Product.java
@@ -1,15 +1,20 @@
 package com.example.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Product extends BaseModel {
 
     private String title;
-    private String description;
+    //private String description;
     private double price;
+    @ManyToOne
     private Category category;
 
 
